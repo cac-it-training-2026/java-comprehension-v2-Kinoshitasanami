cf10953b07777eb2580c53f97a32cc9a4ad7203c
sanamiki <sanamiki@C010422801.zipangu.cac.co.jp>
2026-04-28 16:08:57 +0900

    演習4まで完了

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/AbstMember.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/AbstMember.java
@@ -7,6 +7,30 @@ package q01_basic.question05;
  */
 abstract class AbstMember {
 
+	private int id;
+	private String password;
+	private int age;
+	private int rank;
+
+	AbstMember() {
+
+	}
+
+	/**
+	 * @param id
+	 * @param password
+	 * @param age
+	 * @param rank
+	 * @param name
+	 */
+	public AbstMember(int id, String password, int age, int rank, String name) {
+		this.id = id;
+		this.password = password;
+		this.age = age;
+		this.rank = rank;
+		this.name = name;
+	}
+
 	/**
 	 * name protected access
 	 */
@@ -37,6 +61,62 @@ abstract class AbstMember {
 	 */
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	/**
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id セットする id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password セットする password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age セットする age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return rank
+	 */
+	public int getRank() {
+		return rank;
+	}
+
+	/**
+	 * @param rank セットする rank
+	 */
+	public void setRank(int rank) {
+		this.rank = rank;
 	}
 
 }
--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/SystemMain05.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/SystemMain05.java
@@ -5,7 +5,6 @@ public class SystemMain05 {
 	public static void main(String[] args) {
 
 		AbstMember[] members = new AbstMember[2];
-		//TODO ここから実装する
 
 	}
 
